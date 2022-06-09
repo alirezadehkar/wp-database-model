04648dfb54ef9c2b25af62dfe591fa286feec4f2
Alireza Dehkar <alirezadehkar@gmail.com>
2022-06-09 18:59:44 +0430

    first commit

--- src/Database_Model.php
+++ src/Database_Model.php
@@ -11,11 +11,14 @@ abstract class Database_Model
      * Database_Model __construct
      * 
      */
-    public function __construct()
+    public function __construct($table = '')
     {
         global $wpdb;
         $this->wpdb = $wpdb;
         $this->prefix = $this->wpdb->prefix;
+        if(!empty($table)){
+            static::$table = $table;
+        }
     }
 
     /**
